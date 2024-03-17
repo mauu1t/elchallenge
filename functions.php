b7083662af090f5420304e72ac8e6da4215c4ed3
Mihai Dumitrascu <mdumitrascu@updraftplus.com>
2024-03-17 11:22:03 +0200

    cpt and taxonomy - pt 4

--- functions.php
+++ functions.php
@@ -17,24 +17,70 @@ add_action( 'wp_enqueue_scripts', 'el_enqueue_parent_styles' );
 /**
  * Disable admin bar for specific user names
  *
- * @param array $names Array of usernames that should have admin bar disabled
  * @package elchallenge
  */
-function el_check_user_name( $names ) {
+function el_check_user_name() {
+
+	// Usernames for which we'll disable the admin bar.
+	$usernames = array(
+		'wp-test',
+	);
+
 	if ( is_user_logged_in() ) :
 
 		// Get current logged-in user data.
 		$user = wp_get_current_user();
-		return in_array( $user->user_login, $names );
+		return ! in_array( $user->user_login, $usernames, true );
 	endif;
 
-	return false;
+	return true;
 }
+add_filter( 'show_admin_bar', 'el_check_user_name' );
 
-// Usernames for which we'll disable the admin bar.
-$usernames = array(
-	'wp-test',
-);
-if ( el_check_user_name( $usernames ) ) :
-	add_filter( 'show_admin_bar', '__return_false' );
-endif;
+/**
+ * Registers custom post type and taxonomy
+ *
+ * @package elchallenge
+ */
+function el_post_type_init() {
+
+	// Set the labels.
+	$labels = array(
+		'name'          => __( 'Products' ),
+		'singular_name' => __( 'Product' ),
+		'add_new'       => __( 'Add New Product' ),
+		'add_new_item'  => __( 'Add New Product' ),
+		'edit_item'     => __( 'Edit Product' ),
+		'new_item'      => __( 'New Product' ),
+		'all_items'     => __( 'All Products' ),
+		'view_item'     => __( 'View Product' ),
+		'search_items'  => __( 'Search Products' ),
+	);
+
+	// The arguments for our post type.
+	$args = array(
+		'labels'            => $labels,
+		'description'       => 'Product specific data',
+		'public'            => true,
+		'menu_position'     => 5,
+		'supports'          => array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields' ),
+		'has_archive'       => true,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'query_var'         => true,
+	);
+	register_post_type( 'product', $args );
+
+	// Register taxonomy.
+	register_taxonomy(
+		'product-category',
+		'product',
+		array(
+			'hierarchical' => true,
+			'label'        => 'Categories',
+			'query_var'    => true,
+			'rewrite'      => array( 'slug' => 'product-category' ),
+		)
+	);
+}
+add_action( 'init', 'el_post_type_init' );
